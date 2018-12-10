4019a16181eb3295668269a9561dc65ae6119355
Jun Maurin <maurinfo@icloud.com>
2018-12-10 15:46:22 +0900

    hi

---
+++ classic/topbar/assets/examples/js/apps/mailbox.js
@@ -0,0 +1,23 @@
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define('/apps/mailbox', ['jquery'], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(require('jquery'));
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(global.jQuery);
+    global.appsMailbox = mod.exports;
+  }
+})(this, function (_jquery) {
+  'use strict';
+
+  var _jquery2 = babelHelpers.interopRequireDefault(_jquery);
+
+  // (function(document, window, $) {
+
+  (0, _jquery2.default)(document).ready(function () {
+    AppMailbox.run();
+  });
+});
